83bfcb2a463d6e67b9a946c865e5720bf89bb8bc
Rafael-Casemiro <rafaelcasemirophb@gmail.com>
2025-12-19 21:43:58 -0300

    Adicionando lista de carro na página de compra

--- car/urls.py
+++ car/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import register_car
+from .views import register_car, car_list
 
 app_name = 'car'
 
 urlpatterns = [
-    path('register/', register_car, name='register_car')
+    path('register/', register_car, name='register_car'),
+    path('list/', car_list, name='car_list')
 ]
--- car/views.py
+++ car/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.core.paginator import Paginator
 from .forms import CarForm
 from .models import Car
 
@@ -26,12 +27,21 @@ def register_car(request):
 
 
 def car_list(request):
-        cars = Car.objects.all()
+        cars = Car.objects.filter(buyer=None).order_by('-created_at')
+        paginator = Paginator(cars, 10)
+        page_number = request.GET.get("page")
+        page_obj = paginator.get_page(page_number)
+
+        context = {
+                'page_obj': page_obj,
+                'site_title': 'Cars - ',
+                'cars': cars
+        }
 
         return render(
                 request,
-                'car/list_car.html',
-                {'cars': cars}
+                'car/car_list.html',
+                context
         )
         
 
